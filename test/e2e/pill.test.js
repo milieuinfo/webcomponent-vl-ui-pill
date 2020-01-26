46d2be60ff4fe4f8a2e1ba686c0f540e0e043cbe
Tom Coemans <tom.coemans@gmail.com>
2020-01-26 11:45:38 +0100

    setup import

--- test/e2e/pill.test.js
+++ test/e2e/pill.test.js
@@ -1,5 +1,5 @@
 
-const { assert, driver } = require('vl-ui-core').Test;
+const { assert, driver } = require('vl-ui-core').Test.Setup;
 const VlPillPage = require('./pages/vl-pill.page');
 
 describe('vl-pill', async () => {
