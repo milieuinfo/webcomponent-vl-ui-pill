5e1545ff7c4980b7efcc28f527154a9af9207131
Sander Kleykens <sander.kleykens@avnu.be>
2020-02-28 17:13:21 +0100

    Introduceer e2e testen

--- test/e2e/components/vl-pill.js
+++ test/e2e/components/vl-pill.js
@@ -1,10 +1,48 @@
 const { VlElement } = require('vl-ui-core').Test;
+const { By } = require('vl-ui-core').Test.Setup;
 
-class VlPill extends VlElement {  
-    constructor(driver, selector) {
-        super(driver, selector);
+class VlPill extends VlElement {
+    async isClosable() {
+        return this.hasAttribute('closable');
     }
 
+    async isCheckable() {
+        return this.hasAttribute('checkable');
+    }
+
+    async isChecked() {
+        return await this.driver.executeScript('return arguments[0].checked', this);
+    }
+
+    async toggleCheck() {
+        if (await this.isCheckable()) {
+            await this.click();
+        }
+    }
+
+    async close() {
+        if (await this.isClosable()) {
+            const closeButton = await this._getCloseButton();
+            await closeButton.click();
+        }
+    }
+
+    async getContent() {
+        const slot = await this._getContentSlot();
+        return this.getAssignedNodes(slot);
+    }
+
+    async getType() {
+        return this.getAttribute('type');
+    }
+
+    async _getContentSlot() {
+        return this.shadowRoot.findElement(By.css('slot'));
+    }
+
+    async _getCloseButton() {
+        return this.shadowRoot.findElement(By.css('#close'));
+    }
 }
 
 module.exports = VlPill;
--- test/e2e/pill.test.js
+++ test/e2e/pill.test.js
@@ -1,4 +1,3 @@
-
 const { assert, driver } = require('vl-ui-core').Test.Setup;
 const VlPillPage = require('./pages/vl-pill.page');
 
@@ -9,7 +8,50 @@ describe('vl-pill', async () => {
         return vlPillPage.load();
     });
 
-    it("Dummy test om de browsers te doen sluiten", () => {
-    	assert.isTrue(true);
+    it("Als gebruiker kan ik de inhoud van een pill zien", async () => {
+        const pill = await vlPillPage.getStandardPill();
+        const content = await pill.getContent();
+        await assert.equal(content[0].textContent.trim(), 'Optie 1');
+    });
+
+    it("Als gebruiker kan ik pillen zien zonder type en succes, waarschuwing en fout pillen", async () => {
+        const pill = await vlPillPage.getStandardPill();
+        await assert.eventually.isNull(pill.getType());
+
+        const successPill = await vlPillPage.getSuccessPill();
+        await assert.eventually.equal(successPill.getType(), 'success');
+
+        const warningPill = await vlPillPage.getWarningPill();
+        await assert.eventually.equal(warningPill.getType(), 'warning');
+
+        const errorPill = await vlPillPage.getErrorPill();
+        await assert.eventually.equal(errorPill.getType(), 'error');
+    });
+
+    it("Als gebruiker kan ik een closable pill sluiten", async () => {
+        const pill = await vlPillPage.getClosablePill();
+
+        await assert.eventually.isTrue(pill.isClosable());
+        await assert.eventually.isFalse(pill.isCheckable());
+
+        await pill.close();
+
+        const closeResult = await vlPillPage.getCloseResult();
+        await assert.eventually.equal(closeResult.getTextContent(), 'Er werd op de sluitknop gedrukt!');
+    });
+
+    it("Als gebruiker kan ik een checkable pill aan- en uitvinken", async () => {
+        const pill = await vlPillPage.getCheckablePill();
+
+        await assert.eventually.isFalse(pill.isClosable());
+        await assert.eventually.isTrue(pill.isCheckable());
+
+        await assert.eventually.isFalse(pill.isChecked());
+
+        await pill.toggleCheck();
+        await assert.eventually.isTrue(pill.isChecked());
+
+        await pill.toggleCheck();
+        await assert.eventually.isFalse(pill.isChecked());
     });
 });
