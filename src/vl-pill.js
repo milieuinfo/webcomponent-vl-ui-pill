cd754a815ea16122e11577945fb1dfa9e559325f
Sander Kleykens <sander.kleykens@avnu.be>
2020-03-03 13:39:47 +0100

    Refactor Vl(Label)Pill en introduceer end 2 end testen (#53)

--- src/vl-pill.js
+++ src/vl-pill.js
@@ -1,24 +1,15 @@
-import { VlElement, NativeVlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
+import { VlElement, define } from '/node_modules/vl-ui-core/dist/vl-core.js';
 
-export const VlPillElement = (SuperClass) => {
-  return class extends VlElement(SuperClass) {
-    static get _observedAttributes() {
-      return ['type'];
-    }
-  
-    get _classPrefix() {
-      return 'vl-pill--';
-    }
-  
-    _typeChangedCallback(oldValue, newValue) {
-      if (["success", "warning", "error"].indexOf(newValue) >= 0) {
-        this._changeClass(this._element, oldValue, newValue);
-      } else {
-        this._element.classList.remove(this._classPrefix + oldValue);
-      }
-    }
-  }
-};
+/**
+ * Pill gesloten event
+ * @event VlPill#close
+ */
+
+/**
+ * Pill checked event
+ * @event VlPill#check
+ * @property {boolean} checked - Of de pill checked of unchecked werd.
+ */
 
 /**
  * VlPill
@@ -28,20 +19,19 @@ export const VlPillElement = (SuperClass) => {
  * @extends VlElement
  *
  * @property {(success | warning | error)} type - Attribuut bepaalt de soort van pill: succes, probleem of fout.
- * @property {closable} type - Attribuut bepaalt of de pill kan worden verwijderd.
- * @property {checkable} type - Attribuut bepaalt of de pill aangechecked kan worden dmv een checkbox.
- * 
+ * @property {boolean} closable - Attribuut bepaalt of de pill kan worden verwijderd (kan niet in combinatie met checkable gebruikt worden).
+ * @property {boolean} checkable - Attribuut bepaalt of de pill kan worden aangevinkt (kan niet in combinatie met closable gebruikt worden).
+ *
  * @see {@link http://www.github.com/milieuinfo/webcomponent-vl-ui-pill/releases/latest|Release notes}
  * @see {@link http://www.github.com/milieuinfo/webcomponent-vl-ui-pill/issues|Issues}
  * @see {@link https://webcomponenten.omgeving.vlaanderen.be/demo/vl-pill.html|Demo}
  */
-export class VlPill extends VlPillElement(HTMLElement) {
-  static get pillTemplate() {
-    return `
-      <span class="vl-pill">
-        <slot></slot>
-      </span>
-    `;
+export class VlPill extends VlElement(HTMLElement) {
+  static get EVENTS() {
+    return {
+      close: 'close',
+      check: 'check'
+    };
   }
 
   constructor() {
@@ -49,57 +39,162 @@ export class VlPill extends VlPillElement(HTMLElement) {
       <style>
           @import "/src/style.css";
       </style>
-      ${VlPill.pillTemplate}
+      ${VlPill._standardPillTemplate}
     `);
+  }
+
+  /**
+   * Is de pill aangevinkt?
+   *
+   * @returns {boolean} Geeft terug of de pill aangevinkt is. Als de pill niet checkable is, wordt undefined teruggegeven.
+   */
+  get checked() {
+    const checkbox = this._checkbox;
+    if (checkbox) {
+      return !!checkbox.checked;
+    }
+  }
+
+  /**
+   * Vinkt de pill aan of uit. Als de pill niet checkable is, gebeurt er niets.
+   *
+   * @param {boolean} checked - Of de pill aangevinkt moet zijn of niet.
+   */
+  set checked(checked) {
+    const checkbox = this._checkbox;
+    if (checkbox) {
+      checkbox.checked = checked;
+      this._checked();
+    }
+  }
+
+  get _classPrefix() {
+    return 'vl-pill--';
   }
 
   static get _observedAttributes() {
-    return super._observedAttributes.concat(['closable']);
+    return ['type', 'closable', 'checkable'];
   }
 
-  _getPillTemplate() {
-    return this._template(VlPill.pillTemplate);
+  get _pillTemplate() {
+    if (this.hasAttribute('checkable')) {
+      return VlPill._checkablePillTemplate;
+    } else if (this.hasAttribute('closable')) {
+      return VlPill._closablePillTemplate;
+    } else {
+      return VlPill._standardPillTemplate;
+    }
   }
 
-  _getClosablePillTemplate() {
-    return this._template(`
+  static get _standardPillTemplate() {
+    return `
+      <span class="vl-pill">
+        <slot></slot>
+      </span>
+    `;
+  }
+
+  static get _closablePillTemplate() {
+    return `
       <div class="vl-pill vl-pill--closable">
-          <slot></slot>
-        <button class="vl-pill__close" type="button">
+        <slot></slot>
+        <button id="close" class="vl-pill__close" type="button">
           <span class="vl-u-visually-hidden">Verwijderen</span>
         </button>
       </div>
-    `);
-  }
-
-  _closableChangedCallback(oldValue, newValue) {
-    this._shadow.lastElementChild.replaceWith((newValue != undefined ? this._getClosablePillTemplate() : this._getPillTemplate()));
-  }
-}
-
-export class VlLabelPill extends VlPillElement(NativeVlElement(HTMLLabelElement)) {
-  constructor() {
-    super();
-  }
-
-  connectedCallback() {
-    this.classList.add('vl-pill');
-    this.classList.add('vl-pill--checkable');
-    const input = this._inputElement;
-    input.classList.add("vl-pill--checkable__checkbox");
-    input.insertAdjacentHTML('afterend', this._inputStyleElement);
-  }
-
-  get _inputElement() {
-    return this._element.querySelector('input');
-  }
-
-  get _inputStyleElement() {
-    return `
-      <span></span>
     `;
   }
+
+  static get _checkablePillTemplate() {
+    return `
+      <label class="vl-pill vl-pill--checkable" for="checkbox">
+        <input class="vl-pill--checkable__checkbox" type="checkbox" id="checkbox" name="checkbox" value="checked"/>
+        <span></span> <slot></slot>
+      </label>
+    `;
+  }
+
+  get _checkbox() {
+    return this.shadowRoot.querySelector('#checkbox');
+  }
+
+  get _closeButton() {
+    return this.shadowRoot.querySelector('#close');
+  }
+
+  __removeClosableEventListeners() {
+    const closeButton = this._closeButton;
+    if (closeButton) {
+      closeButton.removeEventListener('click', this._closeClicked.bind(this));
+    }
+  }
+
+  __removeCheckableEventListeners() {
+    const checkbox = this._checkbox;
+    if (checkbox) {
+      checkbox.removeEventListener('click', this._checked.bind(this));
+    }
+  }
+
+  __removeEventListeners() {
+    this.__removeClosableEventListeners();
+    this.__removeCheckableEventListeners()
+  }
+
+  __addClosableEventListeners() {
+    const closeButton = this._closeButton;
+    if (closeButton) {
+      closeButton.addEventListener('click', this._closeClicked.bind(this));
+    }
+  }
+
+  __addCheckableEventListeners() {
+    const checkbox = this._checkbox;
+    if (checkbox) {
+      checkbox.addEventListener('click', this._checked.bind(this));
+    }
+  }
+
+  __addEventListeners() {
+    this.__addClosableEventListeners();
+    this.__addCheckableEventListeners();
+  }
+
+  _redraw() {
+    this.__removeEventListeners();
+    this._shadow.lastElementChild.replaceWith(this._template(this._pillTemplate));
+    this.__addEventListeners();
+  }
+
+  _closeClicked() {
+    this.dispatchEvent(new CustomEvent(VlPill.EVENTS.close));
+  }
+
+  _checked() {
+    this.dispatchEvent(new CustomEvent(VlPill.EVENTS.check, {
+      detail: {
+        checked: this.checked
+      }
+    }));
+  }
+
+  _closableChangedCallback() {
+    this._redraw();
+  }
+
+  _checkableChangedCallback() {
+    this._redraw();
+  }
+
+  _typeChangedCallback(oldValue, newValue) {
+    if (["success", "warning", "error"].indexOf(newValue) >= 0) {
+      this._changeClass(this._element, oldValue, newValue);
+    } else {
+      this._element.classList.remove(this._classPrefix + oldValue);
+    }
+  }
+
+
 }
 
 define('vl-pill', VlPill);
-define('vl-label-pill', VlLabelPill, {extends: 'label'});
--- test/e2e/pill.test.js
+++ test/e2e/pill.test.js
@@ -1,4 +1,3 @@
-
 const { assert, driver } = require('vl-ui-core').Test.Setup;
 const VlPillPage = require('./pages/vl-pill.page');
 
@@ -9,7 +8,58 @@ describe('vl-pill', async () => {
         return vlPillPage.load();
     });
 
-    it("Dummy test om de browsers te doen sluiten", () => {
-    	assert.isTrue(true);
+    it("Als gebruiker kan ik de inhoud van een pill zien", async () => {
+        const pill = await vlPillPage.getStandardPill();
+        const content = await pill.getContentSlotNodes();
+        await assert.equal(content[0].textContent.trim(), 'Optie 1');
+    });
+
+    it("Als gebruiker kan ik pillen zien zonder type en succes, waarschuwing en fout pillen", async () => {
+        const pill = await vlPillPage.getStandardPill();
+        await assert.eventually.isFalse(pill.isSuccess());
+        await assert.eventually.isFalse(pill.isWarning());
+        await assert.eventually.isFalse(pill.isError());
+
+        const successPill = await vlPillPage.getSuccessPill();
+        await assert.eventually.isTrue(successPill.isSuccess());
+        await assert.eventually.isFalse(successPill.isWarning());
+        await assert.eventually.isFalse(successPill.isError());
+
+        const warningPill = await vlPillPage.getWarningPill();
+        await assert.eventually.isFalse(warningPill.isSuccess());
+        await assert.eventually.isTrue(warningPill.isWarning());
+        await assert.eventually.isFalse(warningPill.isError());
+
+        const errorPill = await vlPillPage.getErrorPill();
+        await assert.eventually.isFalse(errorPill.isSuccess());
+        await assert.eventually.isFalse(errorPill.isWarning());
+        await assert.eventually.isTrue(errorPill.isError());
+    });
+
+    it("Als gebruiker kan ik een closable pill sluiten", async () => {
+        const pill = await vlPillPage.getClosablePill();
+
+        await assert.eventually.isTrue(pill.isClosable());
+        await assert.eventually.isFalse(pill.isCheckable());
+
+        await pill.close();
+
+        const closeResult = await vlPillPage.getCloseResult();
+        await assert.eventually.equal(closeResult.getTextContent(), 'Er werd op de sluitknop gedrukt!');
+    });
+
+    it("Als gebruiker kan ik een checkable pill aan- en uitvinken", async () => {
+        const pill = await vlPillPage.getCheckablePill();
+
+        await assert.eventually.isFalse(pill.isClosable());
+        await assert.eventually.isTrue(pill.isCheckable());
+
+        await assert.eventually.isFalse(pill.isChecked());
+
+        await pill.toggleCheck();
+        await assert.eventually.isTrue(pill.isChecked());
+
+        await pill.toggleCheck();
+        await assert.eventually.isFalse(pill.isChecked());
     });
 });
