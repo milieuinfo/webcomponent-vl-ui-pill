badd3294273e7186765bc40038b2fec4b63cbb50
Sander Kleykens <sander.kleykens@avnu.be>
2020-03-03 13:39:47 +0100

    Refactor Vl(Label)Pill en introduceer end 2 end testen (#53)

--- test/e2e/components/vl-pill.js
+++ test/e2e/components/vl-pill.js
@@ -1,10 +1,60 @@
 const { VlElement } = require('vl-ui-core').Test;
+const { By } = require('vl-ui-core').Test.Setup;
 
-class VlPill extends VlElement {  
-    constructor(driver, selector) {
-        super(driver, selector);
+class VlPill extends VlElement {
+    async isClosable() {
+        return this.hasAttribute('closable');
     }
 
+    async isCheckable() {
+        return this.hasAttribute('checkable');
+    }
+
+    async isChecked() {
+        return this.driver.executeScript('return arguments[0].checked', this);
+    }
+
+    async toggleCheck() {
+        if (await this.isCheckable()) {
+            await this.click();
+        }
+    }
+
+    async close() {
+        if (await this.isClosable()) {
+            const closeButton = await this._getCloseButton();
+            await closeButton.click();
+        }
+    }
+
+    async getContentSlotNodes() {
+        const slot = await this._getContentSlot();
+        return this.getAssignedNodes(slot);
+    }
+
+    async isSuccess() {
+        return await this._getType() === 'success';
+    }
+
+    async isWarning() {
+        return await this._getType() === 'warning';
+    }
+
+    async isError() {
+        return await this._getType() === 'error';
+    }
+
+    async _getType() {
+        return this.getAttribute('type');
+    }
+
+    async _getContentSlot() {
+        return this.shadowRoot.findElement(By.css('slot'));
+    }
+
+    async _getCloseButton() {
+        return this.shadowRoot.findElement(By.css('#close'));
+    }
 }
 
 module.exports = VlPill;
--- test/e2e/pages/vl-pill.page.js
+++ test/e2e/pages/vl-pill.page.js
@@ -1,5 +1,5 @@
 const VlPill = require('../components/vl-pill');
-const { Page, Config } = require('vl-ui-core').Test;
+const { Page, Config, VlElement } = require('vl-ui-core').Test;
 
 class VlPillPage extends Page {
     async _getPill(selector) {
@@ -7,7 +7,35 @@ class VlPillPage extends Page {
     }
 
     async load() {
-        await super.load(Config.baseUrl + '/demo/vl-pill.html');
+        await super.load(`${Config.baseUrl}/demo/vl-pill.html`);
+    }
+
+    async getStandardPill() {
+        return this._getPill('#pill');
+    }
+
+    async getSuccessPill() {
+        return this._getPill('#pill-success');
+    }
+
+    async getWarningPill() {
+        return this._getPill('#pill-warning');
+    }
+
+    async getErrorPill() {
+        return this._getPill('#pill-error');
+    }
+
+    async getClosablePill() {
+        return this._getPill('#pill-closable');
+    }
+
+    async getCloseResult() {
+        return new VlElement(this.driver, '#close-result');
+    }
+
+    async getCheckablePill() {
+        return this._getPill('#pill-checkable');
     }
 }
 
