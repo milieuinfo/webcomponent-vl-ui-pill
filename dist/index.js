03a4e4f1216b4035d64752ad7eb287d8de5b107e
Tom Coemans <tom.coemans@gmail.com>
2020-06-04 14:49:04 +0200

    vl-ui-core update trim e2e text

--- dist/index.js
+++ dist/index.js
@@ -1,5 +1,5 @@
-module.exports = { 
-    VlPill: require('../dist/vl-pill.src.js').VlPill,
-    VlPillElement: require('../dist/vl-pill.src.js').VlPillElement,
-    VlLabelPill: require('../dist/vl-pill.src.js').VlLabelPill
-}
+module.exports = {
+  VlPill: require('../dist/vl-pill.src.js').VlPill,
+  VlPillElement: require('../dist/vl-pill.src.js').VlPillElement,
+  VlLabelPill: require('../dist/vl-pill.src.js').VlLabelPill,
+};
